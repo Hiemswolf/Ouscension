06f78c7466a87bf7322cda2c39787f76869de0c0
Lukey <lukemueller@charter.net>
2018-04-13 21:56:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,15 @@ io.on('connection', function(socket){
   });
 });
 
-function setPosition(sprite) { if(createdSelf) {
-    var e = document.getElementById(sprite.element);
-    e.style.left = sprite.x + "px";
-    e.style.top = sprite.y + "px";
-  }
+function createSprite(element, x, y, w, h) {
+  var result = new Object();
+  result.element = element;
+  result.x = x;
+  result.y = y;
+  result.w = w;
+  result.h = h;
+              
+  return result;
 }
 
 function Update() {
