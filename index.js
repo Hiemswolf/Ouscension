569558323538665c6dc78e2946274118c56c7552
Lukey <lukemueller@charter.net>
2018-04-13 21:27:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var port = process.env.PORT || 3000;
 var itemCounter = 0;
 var lastUpdate = new Date().getTime();
 
+var bullets = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -18,6 +20,23 @@ io.on('connection', function(socket){
     io.emit('loadNewChar', socket.number);
   });
   
+  socket.on('createBullet', function(player) {
+    itemCounter++;
+          
+    var bullet = createSprite(itemCounter, player.x + player.w / 2, player.y + player.h / 2, 5, 5);
+    bullet.angle = player.angle;    
+          
+    /*var element = document.createElement("div");
+    element.className = "bullet";
+    element.id = itemCounter;
+    element.style.left = '-50px';
+    element.style.top = '-50px';
+    element.style.transform = "rotate(" + bullet.angle + "deg)";
+    document.getElementById('bulletContainer').appendChild(element);
+          */
+    bullets[bullets.length] = bullet;
+  }
+            
   socket.on('playerInfo', function(val, x , y, angle) {
     io.emit('showPlayer', val, x, y, angle);
   });
