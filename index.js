56ee535b799bd9f73408b60e97d1def716ca92c8
Lukey <lukemueller@charter.net>
2018-04-14 20:40:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ io.on('connection', function(socket){
     bullet.angle = player.angle;    
     bullet.lifeTimer = 40;
     
-    bullet.mx = (Math.floor(player.angle * 1.112) + 100) / 100;
+    bullet.mx = (Math.floor(Math.abs(player.angle) * 1.112) + 100) / 100;
     bullet.my = 1 - bullet.mx;
     
     console.log(bullet.mx);
